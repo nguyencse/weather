ba7d1bc87de59ec48c3ed7423c9dd994d9577ff6
nguyencse <nguyecse.ninhhoa@gmail.com>
2017-05-14 14:54:56 +0700

    complete the bottom view (show info of the temperature, name of the city,...) + set orientation android is only portrait

--- src/index.js
+++ src/index.js
@@ -4,23 +4,23 @@ import {
   Text,
   View,
   StatusBar,
-  Image
+  Image,
+  ListView
 } from 'react-native';
 
 const apiKey = '57bb3d115200197085a58518294d49c2'
 const url = 'http://api.openweathermap.org/data/2.5/weather?'
 const backgroundImage = require('./icons/giphy.gif')
 
-const scatteredclouds = require("./icons/white_cloud.png");
-const clearsky = require("./icons/sunny.png");
-const fewclouds = require("./icons/sunny_intervals.png");
-const brokenclouds = require("./icons/thundery_showers.png");
-const showerrain = require("./icons/heavy_rain_showers.png");
-const rain = require("./icons/cloudy_with_light_rain.png");
-const thunderstorm = require("./icons/thunderstorms.png");
-const snow = require("./icons/cloudy_with_light_snow.png");
-const mist = require("./icons/mist.png");
-const temperature = require("./icons/temperature.png");
+const scatteredclouds = require("./icons/white_cloud.png")
+const clearsky = require("./icons/sunny.png")
+const fewclouds = require("./icons/sunny_intervals.png")
+const brokenclouds = require("./icons/thundery_showers.png")
+const showerrain = require("./icons/heavy_rain_showers.png")
+const rain = require("./icons/cloudy_with_light_rain.png")
+const thunderstorm = require("./icons/thunderstorms.png")
+const snow = require("./icons/cloudy_with_light_snow.png")
+const mist = require("./icons/mist.png")
 
 export default class WeatherIndex extends Component {
   constructor(props){
@@ -57,49 +57,49 @@ export default class WeatherIndex extends Component {
   }
 
   _renderIconWeather(decription){
-    let imageSouce = null;
+    let imageSource = null
     switch(decription){
       case 'clear sky' :
-        imageSouce = clearsky
-        break;
+        imageSource = clearsky
+        break
 
       case 'few clouds':
-        imageSouce = fewclouds;
-        break;
+        imageSource = fewclouds
+        break
 
       case 'scattered clouds':
-        imageSouce = scatteredclouds;
-        break;
+        imageSource = scatteredclouds
+        break
 
       case 'broken clouds':
-        imageSouce = brokenclouds;
+        imageSource = brokenclouds
         break;
 
       case 'shower rain':
-        imageSouce = showerrain;
-        break;
+        imageSource = showerrain
+        break
 
       case 'rain':
-        imageSouce = rain;
+        imageSource = rain
         break;
 
       case 'thunderstorm':
-        imageSouce = thunderstorm;
-        break;
+        imageSource = thunderstorm
+        break
 
       case 'snow':
-        imageSouce = snow;
-        break;
+        imageSource = snow
+        break
 
       case 'mist':
-        imageSouce = mist
-        break;
+        imageSource = mist
+        break
     }
 
     return (
-        <View  key={decription} style={{flexDirection:'row',marginLeft:35}}>
-          <Image style={{width:50,height:50}} source={imageSouce}/>
-          <Text style={{fontSize:15,color:'white',marginTop:10}}>{decription}</Text>
+        <View  key={decription} style={{flexDirection:'row',alignItems:'center'}}>
+        <Text style={{fontSize:15,color:'white',marginTop:10, marginLeft:10}}>{decription}</Text>
+          <Image style={{width:50,height:50}} source={imageSource}/>
         </View>
       )
   }
@@ -117,18 +117,22 @@ export default class WeatherIndex extends Component {
 
         <View style={styles.footer}>
           <View style={styles.footerLeft}>
-            <Text style={styles.tempMax}>{this.state.coords && this.state.coords.main.temp_max}</Text>
-            <Text style={styles.tempO}>o</Text>
-            <Text style={styles.tempC}>C</Text>
+            <View style={styles.temp}>
+              <Text style={styles.tempMax}>{this.state.coords && this.state.coords.main.temp_max}</Text>
+              <Text style={styles.tempOMax}>o</Text>
+              <Text style={styles.tempCMax}>C</Text>
+            </View>
+
+            <View style={styles.temp}>
+              <Text style={styles.tempMin}>{this.state.coords && this.state.coords.main.temp_min}</Text>
+              <Text style={styles.tempOMin}>o</Text>
+              <Text style={styles.tempCMin}>C</Text>
+            </View>
           </View>
 
           <View style={styles.footerRight}>
-            <Text style={styles.placeText}>Ho Chi Minh City</Text>
-            <View style={styles.footer}>
-              {this.state.coords && this.state.coords.weather.map((data)=>this._renderIconWeather(data.description))}
-              <Image style={{width:50,height:50}} source={temperature}/>
-              <Text style={{fontSize:15,color:'white',marginTop:10}}>{this.state.coords && this.state.coords.main.temp_min}</Text>
-          </View>
+            <Text style={styles.placeText}>{this.state.coords && this.state.coords.name}</Text>
+            {this.state.coords && this.state.coords.weather.map((item)=>this._renderIconWeather(item.description))}
           </View>
         </View>
       </Image>
@@ -147,36 +151,54 @@ const styles = StyleSheet.create({
   },
   footer:{
     flex:1,
-    flexDirection:'row',
+    flexDirection:'row'
   },
   containerBody:{
     flex:1,
-    margin:30,
+    marginLeft:30,
+    marginRight:30,
+    marginTop:30,
+    marginBottom:10,
     borderRadius:10,
     backgroundColor:'rgba(0,0,0,0.5)'
   },
   footerLeft:{
     flex:2,
-    flexDirection:'row',
     marginLeft:30,
-    marginTop:20
+    justifyContent:'center'
   },
   footerRight:{
     flex:3,
     marginRight:30,
-    alignItems:'flex-end'
+    alignItems:'flex-end',
+    justifyContent:'center'
+  },
+  temp:{
+    flexDirection:'row'
   },
   tempMax:{
-    fontSize:35,
+    fontSize:40,
+    color:'white'
+  },
+  tempOMax:{
+    fontSize:25,
+    color:'white'
+  },
+  tempCMax:{
+    fontSize:40,
+    color:'white'
+  },
+  tempMin:{
+    fontSize:20,
     color:'white',
     marginBottom:30
   },
-  tempO:{
-    fontSize:20,
+  tempOMin:{
+    fontSize:10,
     color:'white'
   },
-  tempC:{
-    fontSize:35,
+  tempCMin:{
+    fontSize:20,
     color:'white',
     marginBottom:30,
   },
