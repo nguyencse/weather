a6597cdc7e6d74d8d9a82c88b260ab4eea8c5542
nguyencse <nguyecse.ninhhoa@gmail.com>
2017-05-15 12:47:49 +0700

    uninstall module `react-native-popup-dialog`

--- index.android.js
+++ index.android.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import WeatherIndex from './src/index'
+import WeatherIndex from './src/components/index'
 
 AppRegistry.registerComponent('Weather', () => WeatherIndex);
