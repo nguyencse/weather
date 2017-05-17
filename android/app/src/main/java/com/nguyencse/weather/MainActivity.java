cb0e59131833e3b4c9e02ce62e5ad6ddcb96ff3f
nguyencse <nguyecse.ninhhoa@gmail.com>
2017-05-17 16:43:40 +0700

    change package name to `com.nguyencse.weather`

--- android/app/src/main/java/com/nguyencse/weather/MainActivity.java
+++ android/app/src/main/java/com/nguyencse/weather/MainActivity.java
@@ -1,4 +1,4 @@
-package com.weather;
+package com.nguyencse.weather;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/nguyencse/weather/MainApplication.java
+++ android/app/src/main/java/com/nguyencse/weather/MainApplication.java
@@ -1,4 +1,4 @@
-package com.weather;
+package com.nguyencse.weather;
 
 import android.app.Application;
 
